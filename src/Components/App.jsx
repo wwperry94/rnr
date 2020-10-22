fd0a63e4758c25f88b4c81ad81ca790ba7719cd0
wwperry94 <wwperry94@gmail.com>
2020-10-22 18:15:09 -0500

    styling changes

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -26,25 +26,28 @@ export default class App extends Component {
     render() {
         return (
             <Router>
-                    <Fragment>
-                        <div class="shadow p-3 mb-5 bg-white rounded">
-                            <nav className="navbar navbar-dark bg-dark">
-                                <Link to="/films" className="btn btn-secondary" style={{ marginLeft: "2em", marginTop: "1em", marginBottom: "1em", fontSize: "25px", borderRadius: "20px" }}>Go to Films</Link>
-                                <Link to="/People" className="btn btn-secondary" style={{ marginRight: "2em", marginTop: "1em", marginBottom: "1em", fontSize: "25px", borderRadius: "20px" }}>Go to People</Link>
-                            </nav>
-                        </div>
-                        <div style={{ display: "flex", justifyContent: "center" }}>
-                            <img src={logo} alt={"this is the logo"} ></img>
-                        </div>
-                        <br />
-                        <Switch>
-                            <Route path="/films/:id" component={SingleFilm} />
-                            <Route path="/people/:id" component={SinglePerson} />
-                            <Route path="/films" component={Films} />
-                            <Route path="/people" component={People} />
+                <Fragment>
+                    <div class="shadow p-3 mb-5 bg-white rounded">
+                        <nav className="navbar navbar-dark bg-dark">
+                            <Link to="/films" className="btn btn-secondary" style={{ marginLeft: "2em", marginTop: "1em", marginBottom: "1em", fontSize: "25px", borderRadius: "20px" }}>Go to Films</Link>
+                            <Link to="/People" className="btn btn-secondary" style={{ marginRight: "2em", marginTop: "1em", marginBottom: "1em", fontSize: "25px", borderRadius: "20px" }}>Go to People</Link>
+                        </nav>
+                    </div>
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <img src={logo} alt={"this is the logo"} ></img>
+                    </div>
+                    <br />
+                    <div class="alert alert-dark" role="alert" style={{display: "flex", justifyContent: "center"}}>
+                        Welcome, and enjoy these useless facts!
+                    </div>
+                    <Switch>
+                        <Route path="/films/:id" component={SingleFilm} />
+                        <Route path="/people/:id" component={SinglePerson} />
+                        <Route path="/films" component={Films} />
+                        <Route path="/people" component={People} />
 
-                        </Switch>
-                    </Fragment>
+                    </Switch>
+                </Fragment>
             </Router>
         )
 
